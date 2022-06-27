e90060640f9fda484cf12b02aaf7166d4f7cfc4e
gad gad <g.gad@nu.edu.eg>
2022-06-27 14:37:39 -0400

    .

--- src/Data_collection/button.py
+++ src/Data_collection/button.py
@@ -1,34 +1,55 @@
 import wiringpi
+import time 
 
 # initialize
 wiringpi.wiringPiSetup()
 
 # define GPIO mode
-GPIO23 = 4
-GPIO24 = 5
+button_pin = 4
+led_pin = 5
 LOW = 0
 HIGH = 1
 OUTPUT = 1
 INPUT = 0
 PULL_DOWN = 1
-wiringpi.pinMode(GPIO23, OUTPUT)  # LED
-wiringpi.pinMode(GPIO24, INPUT)  # push button
-wiringpi.pullUpDnControl(GPIO24, PULL_DOWN)  # pull down
+wiringpi.pinMode(button_pin, OUTPUT)  # LED
+wiringpi.pinMode(led_pin, INPUT)  # push button
+wiringpi.pullUpDnControl(led_pin, PULL_DOWN)  # pull down
 
 
 # make all LEDs off
 def clear_all():
-    wiringpi.digitalWrite(GPIO23, LOW)
+    wiringpi.digitalWrite(button_pin, LOW)
+
+def get_state(pin): 
+    return wiringpi.digitalRead(pin) 
+
+def long_pressed(pin, long_press_threshold = 3) : 
+    t1 = time.time() 
+    while get_state(pin) == True : 
+        if time.time() - t1 > long_press_threshold : 
+            return True 
+    return False 
+
+def turn_on(pin): 
+    wiringpi.digitalWrite(pin, True) 
+
+def turn_on(pin): 
+    wiringpi.digitalWrite(pin, False) 
+
+def change_state(pin): 
+    state = get_state(pin) 
+    wiringpi.digitalWrite(pin, not state)  
+
 
 try:
     clear_all()
-    while 1:
-        button_state = wiringpi.digitalRead(GPIO24)
-        led_state = wiringpi.digitalRead(GPIO23)
-        if button_state == 1:
-            wiringpi.digitalWrite(GPIO23, not led_state)
+    while True : 
+        if get_state(button_pin) == True: 
+            if long_pressed(button_pin) : 
+                change_state(led_pin) 
 
-        wiringpi.delay(200)
+        wiringpi.delay(10)
 
 except KeyboardInterrupt:
     clear_all()
